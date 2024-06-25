5b20ea439f357868e7da953dc67a25c3e0dc4bd8
debanwita <debanwita4dt@gmail.com>
2024-06-26 00:39:14 +0530

    Adding BasicAuthentication

---
+++ src/main/java/com/in28minutes/rest/webservices/restful/web/services/basic/auth/AuthenticationBean.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restful.web.services.basic.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationBean {
+	private String message;
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restful/web/services/basic/auth/BaicAuthenticationController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservices.restful.web.services.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//Controller
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class BaicAuthenticationController{
+	@GetMapping(path="/basicauth")
+	public AuthenticationBean helloWorldBean() {
+		return new AuthenticationBean("You are authenticated !");
+	}
+	
+}
